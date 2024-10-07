ddb56d638bb9a4474db26551a9692374f9353af3
EC2 Default User <ec2-user@ip-172-31-47-176.eu-central-1.compute.internal>
2024-10-07 08:55:16 +0000

    new

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -13,35 +13,36 @@ provider "aws" {
 }
 
 resource "aws_vpc" "test-app-vpc" {
-  cidr_block = "10.90.0.0/16"
+  cidr_block           = "10.90.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "test-app-vpc"
+  }
 }
 
 resource "aws_subnet" "public-1" {
-  vpc_id     = aws_vpc.test-app-vpc.id
-  cidr_block = "10.90.10.0/24"
+  vpc_id            = aws_vpc.test-app-vpc.id
+  cidr_block        = "10.90.10.0/24"
   availability_zone = "us-east-1a"
-
   tags = {
     Name = "public-1"
   }
 }
 
 resource "aws_subnet" "public-2" {
-  vpc_id     = aws_vpc.test-app-vpc.id
-  cidr_block = "10.90.20.0/24"
+  vpc_id            = aws_vpc.test-app-vpc.id
+  cidr_block        = "10.90.20.0/24"
   availability_zone = "us-east-1b"
-
   tags = {
     Name = "public-2"
   }
 }
 
 resource "aws_subnet" "private-1" {
-  vpc_id     = aws_vpc.test-app-vpc.id
-  cidr_block = "10.90.11.0/24"
+  vpc_id            = aws_vpc.test-app-vpc.id
+  cidr_block        = "10.90.11.0/24"
   availability_zone = "us-east-1a"
-
-
   tags = {
     Name = "private-1"
   }
@@ -49,253 +50,159 @@ resource "aws_subnet" "private-1" {
 
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.test-app-vpc.id
-
   tags = {
     Name = "igw"
   }
 }
 
-resource "aws_route_table" "public-1" {
+resource "aws_route_table" "public" {
   vpc_id = aws_vpc.test-app-vpc.id
-
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw.id
   }
-
-  
-
   tags = {
-    Name = "public-1-route-table"
+    Name = "public-route-table"
   }
 }
 
-resource "aws_route_table" "public-2" {
-  vpc_id = aws_vpc.test-app-vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
-  
-  tags = {
-    Name = "public-2-route-table"
-  }
-}
-
-# Public Subnet 1 ve Route Table 1 ilişkilendirilmesi
 resource "aws_route_table_association" "public-1-association" {
   subnet_id      = aws_subnet.public-1.id
-  route_table_id = aws_route_table.public-1.id
+  route_table_id = aws_route_table.public.id
 }
 
-# Public Subnet 2 ve Route Table 2 ilişkilendirilmesi
 resource "aws_route_table_association" "public-2-association" {
   subnet_id      = aws_subnet.public-2.id
-  route_table_id = aws_route_table.public-2.id
+  route_table_id = aws_route_table.public.id
 }
 
-# Elastic IP oluşturma
 resource "aws_eip" "nat_eip" {
   vpc = true
 }
 
-# NAT Gateway oluşturma (Public Subnet'te olmalı)
 resource "aws_nat_gateway" "nat_gw" {
   allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.public-1.id  # NAT Gateway'i public subnet'te oluşturuyoruz
-
+  subnet_id     = aws_subnet.public-1.id
   tags = {
     Name = "nat-gateway"
   }
 }
 
-# Private Route Table oluşturma
-resource "aws_route_table" "private-1" {
+resource "aws_route_table" "private" {
   vpc_id = aws_vpc.test-app-vpc.id
-
   route {
-    cidr_block = "0.0.0.0/0" # Internet trafiği için
+    cidr_block = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat_gw.id
   }
-
   tags = {
-    Name = "private-1-route-table"
+    Name = "private-route-table"
   }
 }
 
-# Private Subnet ve Route Table ilişkilendirilmesi
 resource "aws_route_table_association" "private-1-association" {
   subnet_id      = aws_subnet.private-1.id
-  route_table_id = aws_route_table.private-1.id
+  route_table_id = aws_route_table.private.id
 }
 
 resource "aws_security_group" "ec2-sec-grp" {
-  name = "ec2-sec-grp"
-  tags = {
-    Name = "ec2-sec-grp"
+  name        = "ec2-sec-grp"
+  description = "Security group for EC2 instances"
+  vpc_id      = aws_vpc.test-app-vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # SSH erişimini sınırlandırabilirsiniz
   }
 
   ingress {
-    from_port = 0
-    protocol  = "-1"
-    to_port   = 0
-    self = true
-  }
-  
-  ingress {
     from_port   = 80
-    protocol    = "tcp"
     to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     from_port   = 443
-    protocol    = "tcp"
     to_port     = 443
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 3000
     protocol    = "tcp"
-    to_port     = 3000
     cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 5000
-    protocol    = "tcp"
-    to_port     = 5000
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-      from_port = 22
-      protocol = "tcp"
-      to_port = 22
-      cidr_blocks = [ "0.0.0.0/0" ]
-  }
-
-  ingress {
-      from_port = 8080
-      protocol = "tcp"
-      to_port = 8080
-      cidr_blocks = [ "0.0.0.0/0" ]
   }
 
   ingress {
     from_port   = 6443
     to_port     = 6443
     protocol    = "tcp"
-    cidr_blocks = ["10.90.10.0/24", "10.90.20.0/24", "10.90.11.0/24"]
-  }  
+    cidr_blocks = ["10.90.10.0/24", "10.90.20.0/24", "10.90.11.0/24"] # Yalnızca VPC içi erişim
+  }
 
   egress {
-      from_port = 0
-      protocol = -1
-      to_port = 0
-      cidr_blocks = [ "0.0.0.0/0" ]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ec2-sec-grp"
   }
 }
 
-
-
-# 8. EC2 Instance Oluşturma
 resource "aws_instance" "master" {
-  ami           = "ami-0ebfd941bbafe70c6"
-  instance_type = "t3a.medium"
-  subnet_id     = aws_subnet.public-1.id
-  security_groups = [aws_security_group.ec2-sec-grp.name]
-  iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.name
-
+  ami                    = "ami-0ebfd941bbafe70c6"
+  instance_type          = "t3a.medium"
+  key_name               = "test"
+  subnet_id              = aws_subnet.public-1.id
+  security_groups        = [aws_security_group.ec2-sec-grp.id]
+  iam_instance_profile   = aws_iam_instance_profile.ec2_instance_profile.name
 
   root_block_device {
     volume_size = 30
   }
 
-  # Dinamik inventory dosyasını oluştur
-  provisioner "local-exec" {
-    command = <<EOT
-      echo "[master]" > ./inventory
-      echo "${self.public_ip}" >> ./inventory
-    EOT
-  }
-
-  provisioner "local-exec" {
-    command = "ansible-playbook -i ./inventory AI-Aplication/Ansible/playbooks/master_setup.yml"
-  }
-
   tags = {
-    Name = "master"
+    Name = "K8s-Master"
   }
 }
 
-resource "aws_instance" "worker-1" {
-  ami           = "ami-0ebfd941bbafe70c6"
-  instance_type = "t3a.medium"
-  subnet_id     = aws_subnet.public-1.id
-  security_groups = [aws_security_group.ec2-sec-grp.name]
-  iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.name
-
+resource "aws_instance" "workerone" {
+  ami                    = "ami-0ebfd941bbafe70c6"
+  instance_type          = "t3a.medium"
+  key_name               = "test"
+  subnet_id              = aws_subnet.public-1.id
+  security_groups        = [aws_security_group.ec2-sec-grp.id]
+  iam_instance_profile   = aws_iam_instance_profile.ec2_instance_profile.name
 
   root_block_device {
     volume_size = 30
   }
 
-  # Worker node'ları inventory dosyasına ekle
-  provisioner "local-exec" {
-    command = <<EOT
-      echo "[workers]" >> ./inventory
-      echo "${self.public_ip}" >> ./inventory
-    EOT
-  }
-
-  provisioner "local-exec" {
-    command = "ansible-playbook -i ./inventory AI-Aplication/Ansible/playbooks/worker_setup.yml"
-  }
-
   tags = {
-    Name = "worker-1"
+    Name = "K8s-Worker-1"
   }
 }
 
-resource "aws_instance" "worker-2" {
-  ami           = ami-0ebfd941bbafe70c6
-  instance_type = "t3a.medium"
-  subnet_id     = aws_subnet.public-2.id
-  security_groups = [aws_security_group.ec2-sec-grp.name]
-  iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.name
+resource "aws_instance" "workertwo" {
+  ami                    = "ami-0ebfd941bbafe70c6"
+  instance_type          = "t3a.medium"
+  key_name               = "test"
+  subnet_id              = aws_subnet.public-2.id
+  security_groups        = [aws_security_group.ec2-sec-grp.id]
+  iam_instance_profile   = aws_iam_instance_profile.ec2_instance_profile.name
 
-
-  # 30 GB EBS Volume
   root_block_device {
     volume_size = 30
-    
   }
- # Worker node'ları inventory dosyasına ekle
-  provisioner "local-exec" {
-    command = <<EOT
-      echo "${self.public_ip}" >> ./inventory
-    EOT
-  }
-
-  provisioner "local-exec" {
-    command = "ansible-playbook -i ./inventory AI-Aplication/Ansible/playbooks/worker_setup.yml"
-  }
-  # # User Data - Shell Script ile Kurulum
-  # user_data = file("setup.sh")
 
   tags = {
-    Name = "worker-2"
-  }
+    Name = "K8s-Worker-2"
+  }
 }
 
 resource "aws_db_subnet_group" "rds_subnet_group" {
   name       = "rds-subnet-group"
-  subnet_ids = [aws_subnet.private-1.id]  # Özel subnet'i kullanıyoruz
-
+  subnet_ids = [aws_subnet.private-1.id]
   tags = {
     Name = "RDS subnet group"
   }
@@ -303,22 +210,22 @@ resource "aws_db_subnet_group" "rds_subnet_group" {
 
 resource "aws_db_instance" "default" {
   allocated_storage    = 10
-  db_name              = "deid"                 # MYSQL_DATABASE
+  db_name              = "deid"
   engine               = "mysql"
   engine_version       = "8.0"
   instance_class       = "db.t3.micro"
-  username             = "raife"                # MYSQL_USER
-  password             = "123"                  # MYSQL_PASSWORD
+  username             = "raife"
+  password             = "123"
   parameter_group_name = "default.mysql8.0"
   skip_final_snapshot  = true
-  publicly_accessible  = false                  # Özel subnet, internet erişimi yok
+  publicly_accessible  = false
   port                 = 3306
+  subnet_group_name    = aws_db_subnet_group.rds_subnet_group.name
+  vpc_security_group_ids = [aws_security_group.rds_security_group.id]
 
-  # RDS Subnet Grubu
-  subnet_group_name = aws_db_subnet_group.rds_subnet_group.name
-
-  # Güvenlik Grubu (Varsayılan bir güvenlik grubu eklenebilir veya özelleştirilebilir)
-  vpc_security_group_ids = [aws_security_group.rds_security_group.id]  # Güvenlik grubu ID'si
+  tags = {
+    Name = "MySQL-DB"
+  }
 }
 
 resource "aws_security_group" "rds_security_group" {
@@ -326,18 +233,17 @@ resource "aws_security_group" "rds_security_group" {
   description = "Allow MySQL traffic"
   vpc_id      = aws_vpc.test-app-vpc.id
 
-  # Ingress rule for MySQL (port 3306) allowing internal VPC access
   ingress {
     from_port   = 3306
     to_port     = 3306
     protocol    = "tcp"
-    cidr_blocks = ["10.90.0.0/16"]  # VPC içi erişim
+    cidr_blocks = ["10.90.0.0/16"]
   }
 
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "-1"  # Allow all outbound traffic
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -354,8 +260,6 @@ resource "aws_ecr_repository" "backend_repo" {
 resource "aws_ecr_repository" "frontend_repo" {
   name = "frontend-repo"
 }
-
-
 
 resource "aws_iam_role" "ec2_role" {
   name = "ec2-ecr-role"
@@ -411,3 +315,17 @@ resource "aws_iam_instance_profile" "ec2_instance_profile" {
   role = aws_iam_role.ec2_role.name
 }
 
+output "master_public_ip" {
+  value = aws_instance.master.public_ip
+  description = "Public IP address of the master instance"
+}
+
+output "workerone_public_ip" {
+  value = aws_instance.workerone.public_ip
+  description = "Public IP address of the first worker instance"
+}
+
+output "workertwo_public_ip" {
+  value = aws_instance.workertwo.public_ip
+  description = "Public IP address of the second worker instance"
+}
